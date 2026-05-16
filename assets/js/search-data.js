c8cf92d5a61f6a5c7d19b864b1b31ffc95206b6d
demiller365 <283874433+demiller365@users.noreply.github.com>
2026-05-16 07:10:59 +0000

    Deploying to gh-pages from @ demiller365/demiller365.github.io@c7bb7c524caf254bff53c93ca3f7dc3e5f8495fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "Projects",
-          description: "Projects I&#39;m proud of.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
